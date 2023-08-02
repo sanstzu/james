db9d8af4898a0a0dbc2fbf737ede56980d999784
sans <clafernalo@gmail.com>
2023-08-03 02:05:34 +0800

    fix: updated sticker title

--- handlers/initialize.go
+++ handlers/initialize.go
@@ -96,8 +96,8 @@ func Initialize(c tele.Context) error {
 
 	stickerSet := &fnc.CreateNewStickerSetParams{
 		UserId:        c.Message().Sender.ID,
-		Name:          stickerName + ":: made with @" + c.Bot().Me.Username,
-		Title:         args["title"],
+		Name:          stickerName,
+		Title:         args["title"] + "[made with" + c.Bot().Me.Username + "]",
 		Stickers:      []fnc.InputSticker{*startingSticker},
 		StickerFormat: "static",
 		StickerType:   "regular",
